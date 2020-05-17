92ad9996fc0f20bdb570fbb1de868c85875d94ef
Mmati1996 <mat.duk.at@gmail.com>
2020-05-17 20:42:53 +0200

    Observer for a driver changes events reimplemented

--- src/main/java/edu/kis/powp/jobs2d/drivers/DriverManager.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/DriverManager.java
@@ -2,12 +2,15 @@ package edu.kis.powp.jobs2d.drivers;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.LoggerDriver;
+import edu.kis.powp.observer.Publisher;
 
 /**
  * Driver manager provides means to setup the driver. It also enables other
  * components and features of the application to react on configuration changes.
  */
 public class DriverManager {
+
+	Publisher publisher = new Publisher();
 
 	private Job2dDriver currentDriver = new LoggerDriver();
 
@@ -16,6 +19,7 @@ public class DriverManager {
 	 */
 	public synchronized void setCurrentDriver(Job2dDriver driver) {
 		currentDriver = driver;
+		publisher.notifyObservers();
 	}
 
 	/**
@@ -23,5 +27,9 @@ public class DriverManager {
 	 */
 	public synchronized Job2dDriver getCurrentDriver() {
 		return currentDriver;
+	}
+
+	public Publisher getPublisher() {
+		return publisher;
 	}
 }
--- src/main/java/edu/kis/powp/jobs2d/features/DriverFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/DriverFeature.java
@@ -4,8 +4,9 @@ import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.drivers.SelectDriverMenuOptionListener;
+import edu.kis.powp.observer.Subscriber;
 
-public class DriverFeature {
+public class DriverFeature implements Subscriber {
 
 	private static DriverManager driverManager = new DriverManager();
 	private static Application app;
@@ -33,6 +34,12 @@ public class DriverFeature {
 	public static void addDriver(String name, Job2dDriver driver) {
 		SelectDriverMenuOptionListener listener = new SelectDriverMenuOptionListener(driver, driverManager);
 		app.addComponentMenuElement(DriverFeature.class, name, listener);
+	}
+
+
+	@Override
+	public void update() {
+		updateDriverInfo();
 	}
 
 	/**
